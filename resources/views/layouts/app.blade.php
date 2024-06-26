c9476e3c26068e2c73e80d91cff1c3e9c6c4ffe6
Fahadjrdev <anonnyaishan@gmail.com>
2024-06-27 04:14:06 +0600

    Logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,6 +99,10 @@
                         });
                     }
                 });
+
+                function logout() {
+                    document.getElementById('logoutForm').submit();
+                }
             });
         </script>
     </body>
